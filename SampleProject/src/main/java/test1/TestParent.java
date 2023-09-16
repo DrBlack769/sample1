7f6bc3baaa89568df51f81aee550e832baa98067
LENOVO <LENOVO@DESKTOP-771B655>
2023-09-16 20:46:46 +0530

    TestProject

---
+++ SampleProject/src/main/java/test1/TestParent.java
@@ -0,0 +1,14 @@
+package test1;
+
+public class TestParent {
+	public void parent() {
+		System.out.println("Hello World");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ TestParent abc = new TestParent();
+ abc.parent();
+	}
+
+}
